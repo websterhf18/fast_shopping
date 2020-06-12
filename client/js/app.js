5e1e1c45af88c25a7ccd076906606bf4cba0bc0d
Hugo Garcia <websterh18@gmail.com>
2020-06-12 00:44:24 -0300

    Update project

---
+++ client/js/app.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+const Index = () => {
+  return <div>Welcome to React222!</div>;
+};
+ReactDOM.render(<Index />, document.getElementById('root'));
