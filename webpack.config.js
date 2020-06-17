36421739adbe6174a144b6d4e4c94960669bc569
Hugo Garcia <websterh18@gmail.com>
2020-06-16 22:49:56 -0300

    Update webpack conf

--- webpack.config.js
+++ webpack.config.js
@@ -6,50 +6,50 @@ const htmlPlugin = new HtmlWebPackPlugin({
   template: "./client/index.html", 
   filename: "./index.html"
 });
-module.exports = () => {
-  const env = dotenv.config().parsed;
-  const envKeys = Object.keys(env).reduce((prev, next) => {
-    prev[`process.env.${next}`] = JSON.stringify(env[next]);
-    return prev;
-  }, {});
-  return {
-    entry: './client/js/index.js',
-    output: { // NEW
-      path: path.resolve(__dirname, 'dist'),
-      filename: "app.bundle.js"
-    }, // NEW Ends
-    resolve: {
-      extensions: ['.js', '.jsx']
-    },
-    devServer: {
-      historyApiFallback: true,
-    },
-    plugins: [
-      htmlPlugin,
-      new webpack.DefinePlugin(envKeys),
-    ],
-    module: {
-      rules: [
-        {
-          test: /\.(js|jsx)$/,
-          exclude: /(node_modules|bower_components)/,
-          use: {
-            loader: 'babel-loader',
-            options: {
-              presets: ['@babel/react', '@babel/env']
-            }
+const env = dotenv.config().parsed;
+const envKeys = Object.keys(env).reduce((prev, next) => {
+  prev[`process.env.${next}`] = JSON.stringify(env[next]);
+  return prev;
+}, {});
+const envConfig = new webpack.DefinePlugin(envKeys);
+
+module.exports ={
+  entry: './client/js/index.js',
+  output: { // NEW
+    path: path.resolve(__dirname, 'dist'),
+    filename: "app.bundle.js"
+  }, // NEW Ends
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  devServer: {
+    historyApiFallback: true,
+  },
+  plugins: [
+    htmlPlugin,
+    envConfig
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/react', '@babel/env']
           }
-        },
-        {
-          test: /\.s?css$/,
-          use: ['style-loader', 'css-loader', 'sass-loader']
-        },
-        {
-          test: /\.(png|svg|jpg|gif)$/,
-          loader: "file-loader",
-          options: { name: '/static/[name].[ext]' }
         }
-      ]
-    }
+      },
+      {
+        test: /\.s?css$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        loader: "file-loader",
+        options: { name: '/static/[name].[ext]' }
+      }
+    ]
   }
 };
